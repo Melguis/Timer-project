a5d8802682f0135a1c173246bbdefee3fb79211d
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2022-07-21 23:50:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Router } from './Router'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
